d9379b67c769b25e47c185996182e7d3508020b1
soonhyeok <saenec8598@gmail.com>
2020-03-13 16:10:14 +0900

    codeit

---
+++ Chap10/09_Adjacency_Matrix.py
@@ -0,0 +1,46 @@
+# 모든 요소를 0으로 초기화시킨 크기 6 x 6 인접 행렬
+adjacency_matrix = [[0 for i in range(6)] for i in range(6)]
+
+adjacency_matrix[0][0] = 0
+adjacency_matrix[0][1] = 1
+adjacency_matrix[0][2] = 1
+adjacency_matrix[0][3] = 0
+adjacency_matrix[0][4] = 0
+adjacency_matrix[0][5] = 0
+
+adjacency_matrix[1][0] = 1
+adjacency_matrix[1][1] = 1
+adjacency_matrix[1][2] = 0
+adjacency_matrix[1][3] = 0
+adjacency_matrix[1][4] = 0
+adjacency_matrix[1][5] = 1
+
+adjacency_matrix[2][0] = 1
+adjacency_matrix[2][1] = 0
+adjacency_matrix[2][2] = 0
+adjacency_matrix[2][3] = 0
+adjacency_matrix[2][4] = 0
+adjacency_matrix[2][5] = 1
+
+adjacency_matrix[3][0] = 0
+adjacency_matrix[3][1] = 1
+adjacency_matrix[3][2] = 0
+adjacency_matrix[3][3] = 0
+adjacency_matrix[3][4] = 1
+adjacency_matrix[3][5] = 1
+
+adjacency_matrix[4][0] = 0
+adjacency_matrix[4][1] = 0
+adjacency_matrix[4][2] = 0
+adjacency_matrix[4][4] = 1
+adjacency_matrix[4][5] = 1
+
+adjacency_matrix[5][0] = 0
+adjacency_matrix[5][1] = 1
+adjacency_matrix[5][2] = 1
+adjacency_matrix[5][3] = 1
+adjacency_matrix[5][4] = 1
+adjacency_matrix[5][5] = 0
+
+
+print(adjacency_matrix)
